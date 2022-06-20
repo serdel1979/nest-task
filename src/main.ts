bc18615aee8d701be2d97098a5280a07ff6f1967
Sergio De Luca <sdlbsso@gmail.com>
2022-06-20 14:21:33 -0300

    Manejo de errores, interceprión de timeout y validaciones

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionFilter } from './common/filters/http-exception.filter';
+import { TomeOutInterceptor } from './common/interceptors/tomeout.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new AllExceptionFilter());
+  app.useGlobalInterceptors(new TomeOutInterceptor())
   await app.listen(3000);
 }
 bootstrap();
--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,37 +1,41 @@
 import { Controller, Delete, Get, Param, Patch, Post, Put, Req, Query, Body, HttpException, HttpStatus, BadRequestException } from '@nestjs/common';
 import { query, Request } from 'express';
 import { resolve } from 'path';
-import { TaskDTO} from './dto/task.dto';
+import { TaskDTO } from './dto/task.dto';
 import { TaskService } from './task.service';
 
 @Controller('api/v1/task')
 export class TaskController {
 
-    constructor(private readonly taskService: TaskService){}
+    constructor(private readonly taskService: TaskService) { }
 
     @Post()
-    create(@Body() taskDTO: TaskDTO){
-        
+    create(@Body() taskDTO: TaskDTO) {
+
+       return new Promise((resolve,reject)=>{
+        setTimeout(()=>reject('3 segundos '),3000)
+       })
+
         //throw new BadRequestException('Error en la petición')
-         
+
         //throw new HttpException('Error en peticion',HttpStatus.BAD_REQUEST);
 
-       
+       /*
         return new Promise((resolve,reject)=>{
             setTimeout(()=>reject('Error en peticion'),5000)
-        })
+        })*/
 
        // return this.taskService.create(taskDTO);
     }
 
 
     @Get()
-    findAll(){
+    findAll() {
         return this.taskService.findAll();
     }
 
     @Get(':id')
-    findOne(@Param() id: string){
+    findOne(@Param() id: string) {
         let ret = this.taskService.findOne(id);
         console.log(ret);
         return ret;
@@ -39,19 +43,19 @@ export class TaskController {
 
 
     @Get('done')
-    methodGet(@Req() req: Request){
+    methodGet(@Req() req: Request) {
         return `method ${req.method}`;
     }
 
     @Put(':id')
-    update(@Param('id') id: string, @Body() taskDTO: TaskDTO){
-        return this.taskService.update(id,taskDTO);
+    update(@Param('id') id: string, @Body() taskDTO: TaskDTO) {
+        return this.taskService.update(id, taskDTO);
     }
 
-   
+
 
     @Delete(':id')
-    delete(@Param('id') id: string){
+    delete(@Param('id') id: string) {
         return this.taskService.delete(id);
     }
 
